c30d5dd5305826bd4add8deb9307d35ee9292a3f
Jens Greiner <jens.greiner@me.com>
2017-07-04 22:53:35 +0200

    added a contract for the future database handling and used some constants defined in contract in EditorActivity for gender types

---
+++ app/src/main/java/com/example/android/pets/data/PetContract.java
@@ -0,0 +1,35 @@
+package com.example.android.pets.data;
+
+import android.provider.BaseColumns;
+
+/**
+ * Contract class for pets database
+ * Created by Jens Greiner on 04.07.17.
+ */
+
+public final class PetContract {
+
+    private PetContract() {
+        // intentionally left blank to prevent others from instantiating the contract class
+    }
+
+    /* Inner class that defines the table contents */
+    public static class PetEntry implements BaseColumns {
+
+        public static final String TABLE_NAME = "pets";
+
+        public static final String _ID = BaseColumns._ID;
+        public static final String COLUMN_NAME = "name";
+        public static final String COLUMN_BREED = "breed";
+        public static final String COLUMN_GENDER = "gender";
+        public static final String COLUMN_WEIGHT = "weight";
+
+        /**
+         * Possible values for the gender of the pet
+         */
+        public static final int GENDER_UNKNOWN = 0;
+        public static final int GENDER_MALE = 1;
+        public static final int GENDER_FEMALE = 2;
+
+    }
+}
